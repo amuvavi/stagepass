3431c4b150d1e6f5e01c0a0a0b3efca527f1cf6d
Austin Muvavi <austin@Austins-MacBook-Air.local>
2025-05-13 13:35:29 +0200

    adding our logo to login and register pages

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,14 +3,14 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
+    <body class="min-h-screen antialiased bg-white dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+        <div class="flex flex-col items-center justify-center gap-6 p-6 bg-background min-h-svh md:p-10">
+            <div class="flex flex-col w-full max-w-sm gap-2">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                    <span class="flex items-center justify-center w-24 h-24 mb-1 rounded-md">
+                       <img src="{{ asset('images/stagepass.png') }}" alt="StagePass Logo" class="w-auto fill-current size-full">
                     </span>
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    <span class="sr-only">{{ config('app.name', 'StagePass') }}</span>
                 </a>
                 <div class="flex flex-col gap-6">
                     {{ $slot }}
